0c3cdc8352389c2078fe43851ff27d51472f25c6
Adam Eisert <adam.eisert@geappliances.com>
2025-03-04 20:33:33 -0500

    has add/stuff

--- HelloWorld/HelloWorld/ContentView.swift
+++ HelloWorld/HelloWorld/ContentView.swift
@@ -7,15 +7,77 @@
 
 import SwiftUI
 
+// remove task
+// edit task
+// save the data to disk using AppStorage
+
+struct ToDoItem: Identifiable {
+    var id: UUID = UUID()
+    var title: String
+    var isComplete: Bool = false
+}
+
 struct ContentView: View {
+    @State private var editingItemId: UUID?
+    @State private var inputTask: String = ""
+    @State private var toDoItems: [ToDoItem] = [ToDoItem(title: "Test")]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                TextField("Input task", text: $inputTask)
+                Button("Add") {
+                    if inputTask.isEmpty { return }
+                    toDoItems.append(ToDoItem(title: inputTask))
+                    inputTask = ""
+                }
+            }
+            .padding([.leading, .trailing, .bottom], 15)
+            .background(Color.gray.opacity(0.2))
+            
+            List {
+                ForEach(toDoItems) { item in
+                    HStack {
+                        Image(systemName: item.isComplete ? "checkmark.circle.fill" : "circle")
+                            .onTapGesture {
+                                if let index = toDoItems.firstIndex(where: { $0.id == item.id}) {
+                                    toDoItems[index].isComplete.toggle()
+                                }
+                            }
+                        if editingItemId == item.id {
+                            TextField("", text: Binding(
+                                get: { item.title },
+                                set: { newValue in
+                                    if let index = toDoItems.firstIndex(where: { $0.id == item.id}) {
+                                        toDoItems[index].title = newValue
+                                    }
+                                }
+                            ))
+                            .onSubmit {
+                                editingItemId = nil
+                            }
+                        } else {
+                                Text(item.title)
+                                .strikethrough(item.isComplete)
+                                .onTapGesture {
+                                    editingItemId = item.id
+                                }
+                        }
+                        Spacer()
+                        Button {
+                            if let index = toDoItems.firstIndex(where: { $0.id == item.id}) {
+                                toDoItems.remove(at: index)
+                            }
+                        } label : {
+                            Image(systemName: "minus.circle")
+                        }
+                        .buttonStyle(BorderlessButtonStyle())
+                    }
+                }
+            }
+            
+            Spacer()
         }
-        .padding()
     }
 }
 
